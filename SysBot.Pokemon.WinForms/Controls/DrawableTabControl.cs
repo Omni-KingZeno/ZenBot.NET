a30dd1154b0e4627a658ba6dce2c464557756f4a
Manu <52102823+Manu098vm@users.noreply.github.com>
2025-08-05 01:48:56 +0200

    Add hover effects for tabs

--- SysBot.Pokemon.WinForms/Controls/DrawableTabControl.cs
+++ SysBot.Pokemon.WinForms/Controls/DrawableTabControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -5,10 +6,44 @@ namespace SysBot.Pokemon.WinForms;
 
 public class DrawableTabControl : TabControl
 {
+    private int _hoveredTabIndex = -1;
+
     public DrawableTabControl()
     {
         DrawMode = TabDrawMode.OwnerDrawFixed;
         SetStyle(ControlStyles.UserPaint, true);
+        this.MouseMove += DrawableTabControl_MouseMove;
+        this.MouseLeave += DrawableTabControl_MouseLeave;
+    }
+
+    private void DrawableTabControl_MouseMove(object? sender, MouseEventArgs e)
+    {
+        for (int i = 0; i < TabCount; i++)
+        {
+            if (GetTabRect(i).Contains(e.Location))
+            {
+                if (_hoveredTabIndex != i)
+                {
+                    _hoveredTabIndex = i;
+                    Invalidate();
+                }
+                return;
+            }
+        }
+        if (_hoveredTabIndex != -1)
+        {
+            _hoveredTabIndex = -1;
+            Invalidate();
+        }
+    }
+
+    private void DrawableTabControl_MouseLeave(object? sender, EventArgs e)
+    {
+        if (_hoveredTabIndex != -1)
+        {
+            _hoveredTabIndex = -1;
+            Invalidate();
+        }
     }
 
     protected override void OnPaint(PaintEventArgs e)
@@ -19,20 +54,16 @@ public class DrawableTabControl : TabControl
         Color pageColor = dark ? Color.FromArgb(32, 32, 32) : Color.White;
         Pen borderPen = dark ? Pens.Black : Pens.Gray;
 
-        // Strip
         int tabHeight = ItemSize.Height;
         Rectangle stripRect = new(0, 0, Width, tabHeight + 2);
         using (var b = new SolidBrush(stripColor))
             e.Graphics.FillRectangle(b, stripRect);
 
-        // Tabs
         for (int i = 0; i < TabCount; i++)
             DrawTab(e.Graphics, i, dark);
 
-        // Lower border
         e.Graphics.DrawLine(borderPen, 0, tabHeight + 1, Width, tabHeight + 1);
 
-        // Page background
         if (SelectedTab != null)
         {
             Rectangle pageRect = DisplayRectangle;
@@ -46,11 +77,14 @@ public class DrawableTabControl : TabControl
     {
         Rectangle rect = GetTabRect(index);
         bool selected = (index == SelectedIndex);
+        bool hovered = (index == _hoveredTabIndex);
 
-        // Tab Colors
         Color tabColor = dark
             ? (selected ? Color.FromArgb(48, 48, 48) : Color.FromArgb(32, 32, 32))
             : (selected ? Color.FromArgb(224, 224, 224) : Color.White);
+
+        if (hovered && !selected)
+            tabColor = dark ? Lighten(tabColor, 0.15f) : Color.FromArgb(240, 240, 240);
 
         Color textColor = dark ? Color.White : Color.Black;
 
@@ -65,5 +99,13 @@ public class DrawableTabControl : TabControl
             textColor,
             TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter
         );
+    }
+
+    private static Color Lighten(Color color, float amount)
+    {
+        int r = color.R + (int)((255 - color.R) * amount);
+        int g = color.G + (int)((255 - color.G) * amount);
+        int b = color.B + (int)((255 - color.B) * amount);
+        return Color.FromArgb(color.A, Math.Min(r, 255), Math.Min(g, 255), Math.Min(b, 255));
     }
 }
