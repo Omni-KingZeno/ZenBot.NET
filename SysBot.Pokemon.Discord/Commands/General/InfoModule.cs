98f21a076790727a8f051205e1aaa843eeab5854
Omni-KingZeno <tacocat4157@gmail.com>
2025-11-29 00:49:27 -0500

    Update info command and Build Versioning

--- SysBot.Pokemon.Discord/Commands/General/InfoModule.cs
+++ SysBot.Pokemon.Discord/Commands/General/InfoModule.cs
@@ -13,9 +13,14 @@ namespace SysBot.Pokemon.Discord;
 public class InfoModule : ModuleBase<SocketCommandContext>
 {
     private const string detail = "I am an open-source Discord bot powered by PKHeX.Core and other open-source software.";
-    private const string repo = "https://github.com/Omni-KingZeno/ZenBot.NET";
-    private const string upstreamRepo = "https://github.com/Manu098vm/ManuBot.NET";
-    private const string upstream = "https://github.com/kwsch/SysBot.NET";
+    private const string pkhexRepo = "https://github.com/kwsch/PKHeX";
+    private const string baseRepo = "https://github.com/kwsch/SysBot.NET";
+    private const string baseALMRepo = "https://github.com/architdate/PKHeX-Plugins";
+    private const string almForkRepo = "https://github.com/santacrab2/PKHeX-Plugins";
+    private const string forkRepo = "https://github.com/Manu098vm/ManuBot.NET";
+    private const string thisRepo = "https://github.com/Omni-KingZeno/ZenBot.NET";
+    private const string version = "v4.7";
+
 
     [Command("info")]
     [Alias("about", "whoami", "owner")]
@@ -27,14 +32,48 @@ public class InfoModule : ModuleBase<SocketCommandContext>
 
         var builder = new EmbedBuilder
         {
+            Title = "Here's a bit about me!",
+            Color = new Color(90, 199, 250),
+        };
+
+        builder.AddField($"{Format.Underline("Info")}",
+            $"- {Format.Bold("ZenBot.NET")}: [Source Code]({thisRepo})\n" +
+            $"- {Format.Bold("Owner")}: {owner}\n" +
+            $"- {Format.Bold("Uptime")}: {GetFormattedUptime(DateTime.Now - Process.GetCurrentProcess().StartTime)}\n" +
+            $"- {Format.Bold("ZenBot Version")}: {version}\n" +
+            $"- {Format.Bold("PKHeX.Core Version")}: {GetSimpleVersionInfo("PKHeX.Core")}\n" +
+            $"- {Format.Bold("AutoLegality Version")}: {GetSimpleVersionInfo("PKHeX.Core.AutoMod")}\n"
+        );
+
+        builder.AddField($"{Format.Underline("Credits")}",
+            $"- {Format.Bold("[Kurt](https://github.com/kwsch)")}: Creation of [PKHeX]({pkhexRepo}) and [SysBot.NET]({baseRepo})\n" +
+            $"- {Format.Bold("[Architdate](https://github.com/architdate)")}: Creation of [Auto Legality Mod(ALM)]({baseALMRepo})\n" +
+            $"- {Format.Bold("[Anubis](https://github.com/Lusamine)")}: Continued Support of SysBot.NET\n" +
+            $"- {Format.Bold("[santacrab](https://github.com/santacrab2)")}: Continued Support of [ALM]({almForkRepo})\n" +
+            $"- {Format.Bold("[Manu](https://github.com/Manu098vm)")}: Creation of the Orginal [Fork]({forkRepo})\n" +
+            $"- {Format.Bold("Countless Others")}: For their support and contributions to these projects!\n"
+        );
+
+        await ReplyAsync(embed: builder.Build()).ConfigureAwait(false);
+    }
+
+    [Command("Legacyinfo")]
+    [Summary("Shows information about the bot")]
+    public async Task LegacyInfoAsync()
+    {
+        var app = await Context.Client.GetApplicationInfoAsync().ConfigureAwait(false);
+        var owner = app.Team?.TeamMembers.FirstOrDefault(member => member.Role == TeamRole.Owner)?.User ?? app.Owner;
+
+        var builder = new EmbedBuilder
+        {
             Color = new Color(114, 137, 218),
             Description = detail,
         };
 
         builder.AddField("Info",
-            $"- [This forks Source Code]({repo})\n" +
-            $"- [Upstream fork Source Code]({upstreamRepo}) by manu098vm\n" +
-            $"- [Upstream Source Code]({upstream}) by kwsch\n" +
+            $"- [This forks Source Code]({thisRepo})\n" +
+            $"- [Upstream fork Source Code]({forkRepo}) by manu098vm\n" +
+            $"- [Upstream Source Code]({baseRepo}) by kwsch\n" +
             $"- Special thanks to Manu and his contributors for the original fork this bot is based on\n" +
             $"- Credit to Kurt, Anubis, and Architdate for developing the original SysBot code.\n" +
             $"- {Format.Bold("Owner")}: {owner} ({owner.Id})\n" +
@@ -80,5 +119,62 @@ public class InfoModule : ModuleBase<SocketCommandContext>
         if (DateTime.TryParseExact(revision, "yyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var buildTime))
             return (inclVersion ? $"{version} " : "") + $@"{buildTime:yy-MM-dd\.hh\:mm}";
         return !inclVersion ? _default : version;
+    }
+
+    public static string GetSimpleVersionInfo(string assemblyName)
+    {
+        const string _default = "Unknown";
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var assembly = assemblies.FirstOrDefault(x => x.GetName().Name == assemblyName);
+        if (assembly is null)
+            return _default;
+
+        var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+        if (attribute is null)
+            return _default;
+
+        var info = attribute.InformationalVersion;
+        var split = info.Split('+');
+        if (split.Length >= 2)
+        {
+            var versionParts = split[0].Split('.');
+            if (versionParts.Length == 3)
+            {
+                var major = versionParts[0].PadLeft(2, '0');
+                var minor = versionParts[1].PadLeft(2, '0');
+                var patch = versionParts[2].PadLeft(2, '0');
+                return $"{major}.{minor}.{patch}";
+            }
+        }
+        return _default;
+    }
+
+    public static string GetFormattedUptime(TimeSpan uptime)
+    {
+        string formattedUptime;
+        if (uptime.TotalDays >= 7)
+        {
+            int weeks = (int)uptime.TotalDays / 7;
+            int remainingDays = (int)uptime.TotalDays % 7;
+
+            formattedUptime = $"{weeks}w {remainingDays}d {uptime.Hours}h {uptime.Minutes}m";
+        }
+        else if (uptime.TotalDays >= 1)
+        {
+            formattedUptime = $"{uptime.Days}d {uptime.Hours}h {uptime.Minutes}m";
+        }
+        else if (uptime.TotalHours >= 1)
+        {
+            formattedUptime = $"{uptime.Hours}h {uptime.Minutes}m";
+        }
+        else if (uptime.TotalMinutes >= 1)
+        {
+            formattedUptime = $"{uptime.Minutes} minute{(uptime.Minutes > 1 ? "s" : "")}";
+        }
+        else
+        {
+            formattedUptime = $"{uptime.Seconds} seconds";
+        }
+        return formattedUptime;
     }
 }
