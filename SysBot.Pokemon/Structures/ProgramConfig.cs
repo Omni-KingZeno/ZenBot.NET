8a68da51eae23a9d43e8c55c9be2ce95fa0817de
Omni-KingZeno <tacocat4157@gmail.com>
2026-09-12 17:34:41 -0400

    Update Version

--- SysBot.Pokemon/Structures/ProgramConfig.cs
+++ SysBot.Pokemon/Structures/ProgramConfig.cs
@@ -8,7 +8,7 @@ public class ProgramConfig : BotList<PokeBotState>
     public PokeTradeHubConfig Hub { get; set; } = new();
     public int Width { get; set; }
     public int Height { get; set; }
-    public static Version Version { get; } = new Version(5, 6, 1);
+    public static Version Version { get; } = new Version(5, 7, 0);
 }
 
 [JsonSerializable(typeof(ProgramConfig))]
